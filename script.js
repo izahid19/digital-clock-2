610b4a195a14ef6af212c62575d37088a2aa48fa
Zahid Mushtaq <mushtaqzahid888@gmail.com>
2023-04-30 17:15:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+function clock(){
+
+    let hours = document.getElementById('hour');
+    let minutes = document.getElementById('minutes');
+    let seconds = document.getElementById('seconds');
+    let ampm = document.getElementById('ampm');
+
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let am = "AM";
+
+    if(h > 12){
+        h = h - 12;
+       let am = "PM"
+    }
+    h = (h < 10) ? "0" + h : h
+    m = (m < 10) ? "0" + m : m
+    s = (s < 10) ? "0" + s : s
+
+
+    hours.innerHTML = h;
+    minutes.innerHTML = m;
+    seconds.innerHTML = s;
+    ampm.innerHTML = am
+
+}
+
+let interval = setInterval(clock, 1000);
